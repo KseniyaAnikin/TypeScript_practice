5c94518790bc022fe0cc527ac9a549b698b4d741
KseniyaAnikin <100955276+KseniyaAnikin@users.noreply.github.com>
2023-02-16 21:00:34 +0300

    add task

---
+++ src/task3.ts
@@ -0,0 +1,97 @@
+export class MyArray<T>  {
+  elements: T[] = [];
+
+  constructor(elements: T[] = [] ) {
+    this.elements = elements;
+  }
+
+  public push(arg: T){
+    if(this.elements !== null && this.elements !== undefined ){
+      if(arg !== null && arg !== undefined )
+      this.elements[this.elements.length] = arg
+    }  
+  }
+
+  public concat(arg: MyArray<T>) {
+    if(this.elements !== null && this.elements !== undefined ){
+      if(arg.elements !== null && arg.elements !== undefined){
+         this.elements = [...this.elements, ...arg.elements]
+      }  
+    }  
+  } 
+
+  public slice(indexstart?: number, indexend?: number): T[]{
+    let test1: T [] = [];
+    if(this.elements !== null && this.elements !== undefined ){ 
+      if(indexstart === undefined && indexend === undefined ){
+        for(let i= 0; i< this.elements.length; i++){
+          let x =this.elements[i];
+          if(x !== null && x !== undefined){
+          test1.push(x)
+          }
+        }
+      }
+      if(indexstart !== undefined && indexend !== undefined ){
+        for(let i= 0; i< indexend-indexstart; i++){
+          let x =this.elements[i+indexstart];
+          if(x !== null && x !== undefined){
+          test1.push(x)
+          }
+        }
+      }
+      if(indexstart !== undefined && indexend !== undefined ){
+        for(let i= 0; i< indexend-indexstart; i++){
+          let x =this.elements[i+indexstart];
+          if(x !== null && x !== undefined){
+          test1.push(x)
+          }
+        }
+      }
+      if(indexstart !== undefined && indexend === undefined ){
+        for(let i= 0; i< this.elements.length; i++){
+          let x =this.elements[i+indexstart];
+          if(x !== null && x !== undefined){
+          test1.push(x)
+          }
+        }
+      }
+    }
+    return test1;
+  }  
+  
+  static areElementsEqual<T>(index1: MyArray<T>, index2: MyArray<T>): boolean {
+    if(index1.elements !== undefined && index2.elements !== undefined ){
+      if(JSON.stringify(index1.elements) !== JSON.stringify(index2.elements)) return false;
+       for(let i = 0; i < index1.elements.length; i++){
+        if(typeof index1.elements[i] === 'object' && typeof index2.elements[i] === 'object'){
+          if(JSON.stringify(index1.elements[i]) !== JSON.stringify(index2.elements[i])) return false;
+        } 
+      }
+    }
+    return true;
+  }
+
+  static flatten<T>(arr: MyArray<T>):T [] {
+    let output: T [] = [];
+    for (let i = 0; i < arr.elements.length; i++) {
+      let x = arr.elements[i];
+      if ( x instanceof MyArray<T> ) {
+        output = output.concat(MyArray.flatten(x));
+      } else {
+        if(x !== undefined)
+        output.push(x);
+      }
+    }
+    return output;
+  }
+ 
+}
+
+const arr = new MyArray<number>([1, 2, 3,4,5]) 
+const arrcop = new MyArray<number>([1, 2, 3, 4,5,6,7]) 
+const arr2 = new MyArray<number>([1, 6, 7, 8, 9, 10, 11])
+const arr3 = new MyArray<number>([1])
+
+
+MyArray.flatten(arr)
+console.log(MyArray.areElementsEqual(arr, arr3))
